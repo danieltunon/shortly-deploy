8f986ca86b269ac92dcdaed3e87931ccba3ab779
danieltunon <danieltunon@gmail.com>
2016-03-31 21:08:53 -0700

    Refactor with mongodb

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,23 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
-});
 
-module.exports = Link;
+var urlSchema = new Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+}, { timestamps: { createdAt: 'created_at'} });
+
+urlSchema.methods.initialize = function() {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+};
+
+var Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;
--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -12,7 +12,7 @@ var Link = require('../app/models/link');
 /////////////////////////////////////////////////////
 
 
-xdescribe('', function() {
+describe('', function() {
 
   beforeEach(function(done) {
     // Log out currently signed in user
@@ -29,7 +29,7 @@ xdescribe('', function() {
       });
   });
 
-  describe('Link creation: ', function() {
+  xdescribe('Link creation: ', function() {
 
     it('Only shortens valid urls, returning a 404 - Not found for invalid urls', function(done) {
       request(app)
@@ -40,7 +40,7 @@ xdescribe('', function() {
         .end(done);
     });
 
-    describe('Shortening links:', function() {
+    xdescribe('Shortening links:', function() {
 
       it('Responds with the short code', function(done) {
         request(app)
@@ -89,7 +89,7 @@ xdescribe('', function() {
 
     }); // 'Shortening Links'
 
-    describe('With previously saved urls: ', function() {
+    xdescribe('With previously saved urls: ', function() {
 
       beforeEach(function(done) {
         link = new Link({
@@ -134,7 +134,7 @@ xdescribe('', function() {
 
   }); // 'Link creation'
 
-  describe('Priviledged Access:', function() {
+  xdescribe('Priviledged Access:', function() {
 
     // /*  Authentication  */
     // // TODO: xit out authentication
